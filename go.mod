12f2d468ee1449a8f09386c32ea5aa3c28a51bf2
anil-intelops <anil@intelops.dev>
2023-06-30 21:01:53 +0530

    added client certs to secrets

--- go.mod
+++ go.mod
@@ -6,11 +6,14 @@ require (
 	github.com/hashicorp/go-version v1.6.0
 	github.com/hashicorp/hc-install v0.5.2
 	github.com/hashicorp/terraform-exec v0.18.1
+	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/pkg/errors v0.9.1
 	github.com/sirupsen/logrus v1.9.2
 	github.com/spf13/cobra v1.7.0
 	github.com/spf13/viper v1.16.0
+	gopkg.in/yaml.v3 v3.0.1
 	helm.sh/helm/v3 v3.12.0
+	k8s.io/api v0.27.2
 	k8s.io/apimachinery v0.27.2
 	k8s.io/client-go v0.27.2
 )
@@ -139,8 +142,6 @@ require (
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
-	k8s.io/api v0.27.2 // indirect
 	k8s.io/apiextensions-apiserver v0.27.1 // indirect
 	k8s.io/apiserver v0.27.1 // indirect
 	k8s.io/cli-runtime v0.27.1 // indirect
--- pkg/cert/generate_certs.go
+++ pkg/cert/generate_certs.go
@@ -2,80 +2,80 @@ package cert
 
 import (
 	"archive/zip"
+	"capten/pkg/config"
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
 	"crypto/x509/pkix"
 	"encoding/pem"
-	"errors"
-	"fmt"
 	"io"
 	"io/ioutil"
 	"math/big"
 	"os"
 	"time"
 
-	"gopkg.in/yaml.v3"
+	"github.com/pkg/errors"
 )
 
-const FILE_PERMISSION os.FileMode = 0755
-const WRITE_FILE_PERMISSION os.FileMode = 0644
-const CERTIFICATE_BITS_4096 = 4096
-const CERTIFICATE_BITS_2048 = 2048
+const (
+	folderPrmission     os.FileMode = 0755
+	filePrmission       os.FileMode = 0644
+	caBitSize                       = 4096
+	certBitSize                     = 2048
+	rootCAKeyFileName               = "root.key"
+	rootCACertFileName              = "root.crt"
+	interCAKeyFileName              = "inter-ca.key"
+	interCACertFileName             = "inter-ca.crt"
+)
 
-type Config struct {
-	Domain     string     `yaml:"Domain"`
-	CertConfig CertConfig `yaml:"CertConfig"`
+func GenerateCerts(captenConfig config.CaptenConfig) error {
+	err := os.MkdirAll(captenConfig.CertPath, folderPrmission)
+	if err != nil {
+		return errors.WithMessagef(err, "failed to create directory %s", captenConfig.CertPath)
+	}
+
+	rootKey, rootCertTemplate, err := generateCACert(captenConfig)
+	if err != nil {
+		return err
+	}
+
+	interKey, interCACertTemplate, err := generateIntermediateCACert(captenConfig, rootKey, rootCertTemplate)
+	if err != nil {
+		return err
+	}
+
+	err = generateAgentCert(captenConfig, interKey, interCACertTemplate)
+	if err != nil {
+		return err
+	}
+
+	err = generateClientCert(captenConfig, interKey, interCACertTemplate)
+	if err != nil {
+		return err
+	}
+
+	if err := generateCACertChain(captenConfig); err != nil {
+		return err
+	}
+
+	if err := generateCaptenClientCertZip(captenConfig); err != nil {
+		return err
+	}
+	return nil
 }
 
-type CertConfig struct {
-	OrgName                    string   `yaml:"OrgName"`
-	RootCertCommonName         string   `yaml:"RootCertCommonName"`
-	IntermediateCertCommonName string   `yaml:"IntermediateCertCommonName"`
-	ServerCertCommonName       string   `yaml:"ServerCertCommonName"`
-	ServerDNSNames             []string `yaml:"ServerDNSNames"`
-	ClientCertCommonName       string   `yaml:"ClientCertCommonName"`
-}
-
-/*
-certDirPath - root path of 'which ends with 'certs'
-configYAMLPath -  captem config yaml path
-*/
-func GenerateCerts(certDirPath, configYAMLPath string) error {
-
-	// Read the YAML file
-	data, err := ioutil.ReadFile(configYAMLPath)
+func generateCACert(captenConfig config.CaptenConfig) (rootKey *rsa.PrivateKey,
+	rootCertTemplate *x509.Certificate, err error) {
+	rootKey, err = rsa.GenerateKey(rand.Reader, caBitSize)
 	if err != nil {
-		errStr := fmt.Sprintf("failed to read YAML capten config file. Error - %vs", err.Error())
-		return errors.New(errStr)
+		err = errors.WithMessage(err, "failed to generate RSA key for root certificate")
+		return
 	}
 
-	// Unmarshal YAML into struct
-	var config Config
-	err = yaml.Unmarshal(data, &config)
-	if err != nil {
-		errStr := fmt.Sprintf("failed to unmarshal YAML config. Error - %vs", err.Error())
-		return errors.New(errStr)
-	}
-
-	// Create directories
-	err = os.MkdirAll(certDirPath, FILE_PERMISSION)
-	if err != nil {
-		errStr := fmt.Sprintf("failed to create root directory. Error - %vs", err.Error())
-		return errors.New(errStr)
-	}
-
-	// Root certificate creation
-	rootKey, err := rsa.GenerateKey(rand.Reader, CERTIFICATE_BITS_4096)
-	if err != nil {
-		errStr := fmt.Sprintf("failed to generate RSA key for root certificate. Error - %vs", err.Error())
-		return errors.New(errStr)
-	}
-
-	rootCertTemplate := x509.Certificate{
+	rootCertTemplate = &x509.Certificate{
 		Subject: pkix.Name{
-			Organization: []string{config.CertConfig.OrgName},
-			CommonName:   config.CertConfig.RootCertCommonName,
+			Organization: []string{captenConfig.OrgName},
+			CommonName:   captenConfig.RootCACommonName,
 		},
 		SerialNumber:          big.NewInt(1),
 		NotBefore:             time.Now(),
@@ -85,37 +85,40 @@ func GenerateCerts(certDirPath, configYAMLPath string) error {
 		BasicConstraintsValid: true,
 	}
 
-	rootCert, err := x509.CreateCertificate(rand.Reader, &rootCertTemplate, &rootCertTemplate, &rootKey.PublicKey, rootKey)
+	rootCert, err := x509.CreateCertificate(rand.Reader, rootCertTemplate, rootCertTemplate, &rootKey.PublicKey, rootKey)
 	if err != nil {
-		errStr := fmt.Sprintf("failed to create root certificate. Error - %vs", err.Error())
-		return errors.New(errStr)
+		err = errors.WithMessage(err, "failed to create root CA certificate")
+		return
 	}
 
 	rootCertPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: rootCert})
-	err = ioutil.WriteFile(certDirPath+"/root-cert.pem", rootCertPEM, WRITE_FILE_PERMISSION)
+	err = ioutil.WriteFile(captenConfig.CertPath+rootCACertFileName, rootCertPEM, filePrmission)
 	if err != nil {
-		errStr := fmt.Sprintf("error while writing from root cert to certs/root-cert.pem. Error - %vs", err.Error())
-		return errors.New(errStr)
+		err = errors.WithMessage(err, "error while writing from root CA cert")
+		return
 	}
 
 	rootKeyPEM := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(rootKey)})
-	err = ioutil.WriteFile(certDirPath+"/root-key.pem", rootKeyPEM, WRITE_FILE_PERMISSION)
+	err = ioutil.WriteFile(captenConfig.CertPath+rootCAKeyFileName, rootKeyPEM, filePrmission)
 	if err != nil {
-		errStr := fmt.Sprintf("error while writing from root key to certs/root-key.pem. Error - %vs", err.Error())
-		return errors.New(errStr)
+		err = errors.WithMessage(err, "error while writing from root CA key")
+		return
+	}
+	return
+}
+
+func generateIntermediateCACert(captenConfig config.CaptenConfig, rootKey *rsa.PrivateKey,
+	rootCertTemplate *x509.Certificate) (interKey *rsa.PrivateKey, interCACertTemplate *x509.Certificate, err error) {
+	interKey, err = rsa.GenerateKey(rand.Reader, caBitSize)
+	if err != nil {
+		err = errors.WithMessage(err, "failed to generate RSA key for intermediate certificate")
+		return
 	}
 
-	// Intermediate certificate creation
-	interKey, err := rsa.GenerateKey(rand.Reader, CERTIFICATE_BITS_4096)
-	if err != nil {
-		errStr := fmt.Sprintf("failed to generate RSA key for intermediate certificate. Error - %vs", err.Error())
-		return errors.New(errStr)
-	}
-
-	interCertTemplate := x509.Certificate{
+	interCACertTemplate = &x509.Certificate{
 		Subject: pkix.Name{
-			Organization: []string{config.CertConfig.OrgName},
-			CommonName:   config.CertConfig.IntermediateCertCommonName,
+			Organization: []string{captenConfig.OrgName},
+			CommonName:   captenConfig.IntermediateCACommonName,
 			Locality:     []string{"agent"},
 		},
 		SerialNumber:          big.NewInt(1),
@@ -126,97 +129,85 @@ func GenerateCerts(certDirPath, configYAMLPath string) error {
 		BasicConstraintsValid: true,
 	}
 
-	interCert, err := x509.CreateCertificate(rand.Reader, &interCertTemplate, &rootCertTemplate, &interKey.PublicKey, rootKey)
+	interCert, err := x509.CreateCertificate(rand.Reader, interCACertTemplate, rootCertTemplate, &interKey.PublicKey, rootKey)
 	if err != nil {
-		errStr := fmt.Sprintf("failed to create intermediate certificate. Error - %vs", err.Error())
-		return errors.New(errStr)
+		err = errors.WithMessage(err, "failed to create intermediate CA certificate")
+		return
 	}
 
 	interCertPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: interCert})
-	err = ioutil.WriteFile(certDirPath+"/ca-cert.pem", interCertPEM, WRITE_FILE_PERMISSION)
+	err = ioutil.WriteFile(captenConfig.CertPath+interCACertFileName, interCertPEM, filePrmission)
 	if err != nil {
-		errStr := fmt.Sprintf("error while writing from intermediate cert to certs/ca-cert.pem. Error - %vs", err.Error())
-		return errors.New(errStr)
+		err = errors.WithMessage(err, "error while writing from intermediate CA cert")
+		return
 	}
 
 	interKeyPEM := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(interKey)})
-	err = ioutil.WriteFile(certDirPath+"/ca-key.pem", interKeyPEM, WRITE_FILE_PERMISSION)
+	err = ioutil.WriteFile(captenConfig.CertPath+interCAKeyFileName, interKeyPEM, filePrmission)
 	if err != nil {
-		errStr := fmt.Sprintf("error while writing from intermediate key to certs/ca-key.pem. Error - %vs", err.Error())
-		return errors.New(errStr)
+		err = errors.WithMessage(err, "error while writing from intermediate CA key")
+		return
+	}
+	return
+}
+
+func generateAgentCert(captenConfig config.CaptenConfig, interKey *rsa.PrivateKey,
+	interCACertTemplate *x509.Certificate) (err error) {
+	agentKey, err := rsa.GenerateKey(rand.Reader, certBitSize)
+	if err != nil {
+		err = errors.WithMessage(err, "failed to generate RSA key for agent certificate")
+		return
 	}
 
-	catCertPEM, err := ioutil.ReadFile(certDirPath + "/root-cert.pem")
-	if err != nil {
-		errStr := fmt.Sprintf("error while reading certs/root-cert.pem file . Error - %vs", err.Error())
-		return errors.New(errStr)
-	}
-
-	err = ioutil.WriteFile(certDirPath+"/ca-cert-chain.pem", append(interCertPEM, catCertPEM...), WRITE_FILE_PERMISSION)
-	if err != nil {
-		errStr := fmt.Sprintf("error while writing to certs/ca-cert-chain.pem file. Error - %vs", err.Error())
-		return errors.New(errStr)
-	}
-
-	err = ioutil.WriteFile(certDirPath+"/root-cert.pem", catCertPEM, WRITE_FILE_PERMISSION)
-	if err != nil {
-		errStr := fmt.Sprintf("error while writing to certs/root-cert.pem file. Error - %vs", err.Error())
-		return errors.New(errStr)
-	}
-
-	// Server certificate creation
-	serverKey, err := rsa.GenerateKey(rand.Reader, CERTIFICATE_BITS_2048)
-	if err != nil {
-		errStr := fmt.Sprintf("failed to generate RSA key for server certificate. Error - %vs", err.Error())
-		return errors.New(errStr)
-	}
-
-	serverCertTemplate := x509.Certificate{
+	agentCertTemplate := x509.Certificate{
 		Subject: pkix.Name{
-			Organization: []string{config.CertConfig.OrgName},
-			CommonName:   config.CertConfig.ServerCertCommonName,
+			Organization: []string{captenConfig.OrgName},
+			CommonName:   captenConfig.AgentCertCommonName,
 		},
 		SerialNumber:          big.NewInt(1),
 		NotBefore:             time.Now(),
 		NotAfter:              time.Now().AddDate(0, 1, 0),
 		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageKeyEncipherment,
 		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
-		DNSNames:              config.CertConfig.ServerDNSNames,
+		DNSNames:              captenConfig.AgentDNSNames,
 		BasicConstraintsValid: true,
 		IsCA:                  false,
 	}
 
-	serverCert, err := x509.CreateCertificate(rand.Reader, &serverCertTemplate, &interCertTemplate, &serverKey.PublicKey, interKey)
+	agentCert, err := x509.CreateCertificate(rand.Reader, &agentCertTemplate, interCACertTemplate, &agentKey.PublicKey, interKey)
 	if err != nil {
-		errStr := fmt.Sprintf("failed to create server certificate. Error - %vs", err.Error())
-		return errors.New(errStr)
+		err = errors.WithMessage(err, "failed to create server certificate")
+		return
 	}
 
-	serverCertPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: serverCert})
-	err = ioutil.WriteFile(certDirPath+"/server.crt", serverCertPEM, WRITE_FILE_PERMISSION)
+	agentCertPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: agentCert})
+	err = ioutil.WriteFile(captenConfig.CertPath+captenConfig.AgentCertFileName, agentCertPEM, filePrmission)
 	if err != nil {
-		errStr := fmt.Sprintf("error while writing from server cert to certs/server.crt. Error - %vs", err.Error())
-		return errors.New(errStr)
+		err = errors.WithMessage(err, "error while writing from agent cert to certs/server.crt")
+		return
 	}
 
-	serverKeyPEM := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(serverKey)})
-	err = ioutil.WriteFile(certDirPath+"/server.key", serverKeyPEM, WRITE_FILE_PERMISSION)
+	agentKeyPEM := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(agentKey)})
+	err = ioutil.WriteFile(captenConfig.CertPath+captenConfig.AgentKeyFileName, agentKeyPEM, filePrmission)
 	if err != nil {
-		errStr := fmt.Sprintf("error while writing from server key to certs/server.key. Error - %vs", err.Error())
-		return errors.New(errStr)
+		err = errors.WithMessage(err, "error while writing from agent key to certs/server.key")
+		return
 	}
+	return
+}
 
-	// Client certificate creation
-	clientKey, err := rsa.GenerateKey(rand.Reader, CERTIFICATE_BITS_2048)
+func generateClientCert(captenConfig config.CaptenConfig, interKey *rsa.PrivateKey,
+	interCACertTemplate *x509.Certificate) (err error) {
+	clientKey, err := rsa.GenerateKey(rand.Reader, certBitSize)
 	if err != nil {
-		errStr := fmt.Sprintf("failed to generate RSA key for client certificate. Error - %vs", err.Error())
-		return errors.New(errStr)
+		err = errors.WithMessage(err, "failed to generate RSA key for capten client certificate")
+		return
 	}
 
 	clientCertTemplate := x509.Certificate{
 		Subject: pkix.Name{
-			Organization: []string{config.CertConfig.OrgName},
-			CommonName:   config.CertConfig.ClientCertCommonName,
+			Organization: []string{captenConfig.OrgName},
+			CommonName:   captenConfig.CaptenClientCertCommonName,
 		},
 		SerialNumber:          big.NewInt(1),
 		NotBefore:             time.Now(),
@@ -227,79 +218,77 @@ func GenerateCerts(certDirPath, configYAMLPath string) error {
 		IsCA:                  false,
 	}
 
-	clientCert, err := x509.CreateCertificate(rand.Reader, &clientCertTemplate, &interCertTemplate, &clientKey.PublicKey, interKey)
+	clientCert, err := x509.CreateCertificate(rand.Reader, &clientCertTemplate, interCACertTemplate, &clientKey.PublicKey, interKey)
 	if err != nil {
-		errStr := fmt.Sprintf("failed to create client certificate. Error - %vs", err.Error())
-		return errors.New(errStr)
+		return errors.WithMessage(err, "failed to create client certificate")
 	}
 
 	clientCertPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: clientCert})
-	err = ioutil.WriteFile(certDirPath+"/client.crt", clientCertPEM, WRITE_FILE_PERMISSION)
+	err = ioutil.WriteFile(captenConfig.CertPath+captenConfig.ClientCertFileName, clientCertPEM, filePrmission)
 	if err != nil {
-		errStr := fmt.Sprintf("error while writing from client cert to certs/client.crt. Error - %vs", err.Error())
-		return errors.New(errStr)
+		return errors.WithMessage(err, "error while writing from client cert to certs/client.crt")
 	}
 
 	clientKeyPEM := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(clientKey)})
-	err = ioutil.WriteFile(certDirPath+"/client.key", clientKeyPEM, WRITE_FILE_PERMISSION)
+	err = ioutil.WriteFile(captenConfig.CertPath+captenConfig.ClientKeyFileName, clientKeyPEM, filePrmission)
 	if err != nil {
-		errStr := fmt.Sprintf("error while writing from client key to certs/client.key. Error - %vs", err.Error())
-		return errors.New(errStr)
+		return errors.WithMessage(err, "error while writing from client key to certs/client.key")
+	}
+	return
+}
+
+func generateCACertChain(captenConfig config.CaptenConfig) error {
+	caCertPEMFromFile, err := ioutil.ReadFile(captenConfig.CertPath + rootCACertFileName)
+	if err != nil {
+		return errors.WithMessage(err, "error while reading root CA cert file")
 	}
 
-	if err := GenerateClientCertZipFile(certDirPath); err != nil {
-		return err
+	interCACertPEMFromFile, err := ioutil.ReadFile(captenConfig.CertPath + interCACertFileName)
+	if err != nil {
+		return errors.WithMessage(err, "error while reading intermediate CA cert file")
 	}
 
+	err = ioutil.WriteFile(captenConfig.CertPath+captenConfig.CAFileName, append(caCertPEMFromFile, interCACertPEMFromFile...), filePrmission)
+	if err != nil {
+		return errors.WithMessage(err, "error while writing to ca cert file")
+	}
 	return nil
 }
 
-func GenerateClientCertZipFile(certDirPath string) error {
-	// Create a new zip file
-	zipFile, err := os.Create(certDirPath + "/server-client-auth-certs.zip")
+func generateCaptenClientCertZip(captenConfig config.CaptenConfig) error {
+	zipFile, err := os.Create(captenConfig.CertPath + captenConfig.ClientCertExportFileName)
 	if err != nil {
-		errStr := fmt.Sprintf("error while creating server-client-auth-certs.zip zip file. Error - %vs", err.Error())
-		return errors.New(errStr)
+		return errors.WithMessage(err, "error while creating client cert export file")
 	}
 	defer zipFile.Close()
 
-	// Create a new zip writer
 	zipWriter := zip.NewWriter(zipFile)
 	defer zipWriter.Close()
 
-	// Add the client cert file to the zip
-	err = addFileToZip(zipWriter, "client.crt", certDirPath+"/client.crt")
+	err = addFileToZip(zipWriter, captenConfig.ClientCertFileName, captenConfig.CertPath+captenConfig.ClientCertFileName)
 	if err != nil {
-		errStr := fmt.Sprintf("error while adding client cert file to zip folder. Error - %vs", err.Error())
-		return errors.New(errStr)
+		return errors.WithMessage(err, "error while adding client cert file to zip")
 	}
 
-	// Add the client key file to the zip
-	err = addFileToZip(zipWriter, "client.key", certDirPath+"/client.key")
+	err = addFileToZip(zipWriter, captenConfig.ClientKeyFileName, captenConfig.CertPath+captenConfig.ClientKeyFileName)
 	if err != nil {
-		errStr := fmt.Sprintf("error while adding client key file to zip folder. Error - %vs", err.Error())
-		return errors.New(errStr)
+		return errors.WithMessage(err, "error while adding client key file to zip")
 	}
 
-	// Add the ca cert chain cert file to the zip
-	err = addFileToZip(zipWriter, "ca-cert-chain.pem", certDirPath+"/ca-cert-chain.pem")
+	err = addFileToZip(zipWriter, captenConfig.CAFileName, captenConfig.CertPath+captenConfig.CAFileName)
 	if err != nil {
-		errStr := fmt.Sprintf("error while adding ca-cert-chain.pem file to zip folder. Error - %vs", err.Error())
-		return errors.New(errStr)
+		return errors.WithMessage(err, "error while adding ca cert file to zip")
 	}
-
 	return nil
 }
 
 func addFileToZip(zipWriter *zip.Writer, fileName, filePath string) error {
-	// Open the file to be added to the zip
 	file, err := os.Open(filePath)
 	if err != nil {
 		return err
 	}
 	defer file.Close()
 
-	// Create a new file header
 	fileInfo, err := file.Stat()
 	if err != nil {
 		return err
@@ -310,17 +299,14 @@ func addFileToZip(zipWriter *zip.Writer, fileName, filePath string) error {
 	}
 	header.Name = fileName
 
-	// Create a new zip file entry
 	entry, err := zipWriter.CreateHeader(header)
 	if err != nil {
 		return err
 	}
 
-	// Write the file data to the zip entry
 	_, err = io.Copy(entry, file)
 	if err != nil {
 		return err
 	}
-
 	return nil
 }
