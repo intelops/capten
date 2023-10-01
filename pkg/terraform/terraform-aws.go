8ed1f5cb36ff62b4660a52c6301d12bdd3c633cb
Shifna12Zarnaz <shifnazarnaz1205@gmail.com>
2023-10-01 20:31:40 +0530

    Added terraform code

---
+++ pkg/terraform/terraform-aws.go
@@ -0,0 +1,136 @@
+package terraform
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/hashicorp/go-version"
+	"github.com/hashicorp/hc-install/product"
+	"github.com/hashicorp/hc-install/releases"
+	"github.com/hashicorp/terraform-exec/tfexec"
+	"github.com/pkg/errors"
+
+	"capten/pkg/clog"
+	"capten/pkg/config"
+	"capten/pkg/types"
+)
+
+type terraform struct {
+	azureconfig  types.AzureClusterInfo
+	config       types.AWSClusterInfo
+	exec         *tfexec.Terraform
+	captenConfig config.CaptenConfig
+}
+
+func New(captenConfig config.CaptenConfig, config types.AWSClusterInfo) (*terraform, error) {
+	installer := &releases.ExactVersion{
+		Product:    product.Terraform,
+		Version:    version.Must(version.NewVersion("1.3.7")),
+		InstallDir: "./",
+	}
+
+	execPath, err := installer.Install(context.Background())
+	if err != nil {
+		clog.Logger.Infof("execPath: %s", execPath)
+
+		return nil, errors.WithMessage(err, "error installing Terraform")
+	}
+
+	workDir := captenConfig.PrepareDirPath(captenConfig.TerraformModulesDirPath + captenConfig.CloudService + "/" + captenConfig.ClusterType)
+	clog.Logger.Debugf("terraform workingDir: %s, execPath: %s", workDir, execPath)
+	tf, err := tfexec.NewTerraform(workDir, execPath)
+	if err != nil {
+		return nil, errors.WithMessage(err, "error running NewTerraform")
+	}
+
+	tf.SetLogger(clog.Logger)
+	//set the output files, defaulted to terminal
+	tf.SetStdout(os.Stdout)
+	tf.SetStderr(os.Stderr)
+	return &terraform{config: config, exec: tf, captenConfig: captenConfig}, nil
+}
+
+func (t *terraform) init() error {
+
+	backendConfigOptionsStr := []string{
+		"region=" + t.config.Region,
+		"access_key=" + t.config.AwsAccessKey,
+		"secret_key=" + t.config.AwsSecretKey,
+	}
+	backendConfigOptionsStr = append(backendConfigOptionsStr, t.config.TerraformBackendConfigs...)
+	initOptions := make([]tfexec.InitOption, 0)
+	for _, backendConfigOption := range backendConfigOptionsStr {
+		initOptions = append(initOptions, tfexec.BackendConfig(backendConfigOption))
+	}
+
+	initOptions = append(initOptions, tfexec.Upgrade(t.captenConfig.TerraformInitUpgrade))
+	initOptions = append(initOptions, tfexec.Reconfigure(t.captenConfig.TerraformInitReconfigure))
+	err := t.exec.Init(context.Background(), initOptions...)
+	if err != nil {
+		return errors.WithMessage(err, "terraform init failed")
+	}
+	return nil
+}
+
+func (t *terraform) Apply() error {
+
+	var err error
+	if isAzureConfigInitialized(t.azureconfig) {
+		err := t.initAzure()
+		if err != nil {
+			return err
+		}
+
+	} else {
+		err := t.init()
+		if err != nil {
+				return err
+			}
+	}
+
+
+	_, err = t.exec.Show(context.Background())
+	if err != nil {
+		return errors.WithMessage(err, "error running show")
+	}
+
+	varFile := fmt.Sprintf("%s%s%s", t.captenConfig.CurrentDirPath, t.captenConfig.TerraformTemplateDirPath, t.captenConfig.TerraformVarFileName)
+	_, err = t.exec.Plan(context.Background(), tfexec.VarFile(varFile))
+	if err != nil {
+		return errors.WithMessage(err, "error running plan")
+	}
+
+	if err := t.exec.Apply(context.Background(), tfexec.VarFile(varFile)); err != nil {
+		return errors.WithMessage(err, "error running apply")
+	}
+	return nil
+}
+func isAzureConfigInitialized(config types.AzureClusterInfo) bool {
+
+	return config.Region != ""
+}
+
+func (t *terraform) Destroy() error {
+	var err error
+	if isAzureConfigInitialized(t.azureconfig) {
+		err := t.initAzure()
+		if err != nil {
+			return err
+		}
+
+	} else {
+		err := t.init()
+		if err != nil {
+				return err
+			}
+	}
+
+	_, err = t.exec.Show(context.Background())
+	if err != nil {
+		return errors.WithMessage(err, "error running show")
+	}
+
+	varFile := fmt.Sprintf("%s%s%s", t.captenConfig.CurrentDirPath, t.captenConfig.TerraformTemplateDirPath, t.captenConfig.TerraformVarFileName)
+	return t.exec.Destroy(context.Background(), tfexec.VarFile(varFile))
+}
---
+++ pkg/terraform/terraform-azure.go
@@ -0,0 +1,65 @@
+package terraform
+
+import (
+	"context"
+
+	"os"
+
+	"github.com/hashicorp/go-version"
+	"github.com/hashicorp/hc-install/product"
+	"github.com/hashicorp/hc-install/releases"
+	"github.com/hashicorp/terraform-exec/tfexec"
+	"github.com/pkg/errors"
+
+	"capten/pkg/clog"
+	"capten/pkg/config"
+	"capten/pkg/types"
+)
+
+func NewAzure(captenConfig config.CaptenConfig, config types.AzureClusterInfo) (*terraform, error) {
+	installer := &releases.ExactVersion{
+		Product:    product.Terraform,
+		Version:    version.Must(version.NewVersion("1.3.7")),
+		InstallDir: "./",
+	}
+
+	execPath, err := installer.Install(context.Background())
+	if err != nil {
+		clog.Logger.Infof("execPath: %s", execPath)
+
+		return nil, errors.WithMessage(err, "error installing Terraform")
+	}
+
+	workDir := captenConfig.PrepareDirPath(captenConfig.TerraformModulesDirPath + captenConfig.CloudService + "/" + captenConfig.ClusterType)
+	clog.Logger.Debugf("terraform workingDir: %s, execPath: %s", workDir, execPath)
+	tf, err := tfexec.NewTerraform(workDir, execPath)
+	if err != nil {
+		return nil, errors.WithMessage(err, "error running NewTerraform")
+	}
+
+	tf.SetLogger(clog.Logger)
+	//set the output files, defaulted to terminal
+	tf.SetStdout(os.Stdout)
+	tf.SetStderr(os.Stderr)
+	return &terraform{azureconfig: config, exec: tf, captenConfig: captenConfig}, nil
+}
+
+func (t *terraform) initAzure() error {
+	backendConfigOptionsStr := []string{
+		"region=" + t.config.Region,
+	}
+	backendConfigOptionsStr = append(backendConfigOptionsStr)
+
+	initOptions := make([]tfexec.InitOption, 0)
+	for _, backendConfigOption := range backendConfigOptionsStr {
+		initOptions = append(initOptions, tfexec.BackendConfig(backendConfigOption))
+	}
+	initOptions = append(initOptions, tfexec.Upgrade(t.captenConfig.TerraformInitUpgrade))
+	initOptions = append(initOptions, tfexec.Reconfigure(t.captenConfig.TerraformInitReconfigure))
+
+	err := t.exec.Init(context.Background(), initOptions...)
+	if err != nil {
+		return errors.WithMessage(err, "terraform init failed")
+	}
+	return nil
+}
