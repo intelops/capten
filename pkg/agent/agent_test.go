f5cef183ccbed738b0d7cdbd97a8327a1f9d68a3
ShifnaZarnaz <shifna@intelops.dev>
2024-06-06 17:39:42 +0530

    Updated the test files

--- pkg/agent/agent_test.go
+++ pkg/agent/agent_test.go
@@ -4,17 +4,33 @@ import (
 	"capten/pkg/agent/pb/agentpb"
 	"capten/pkg/agent/pb/vaultcredpb"
 	"capten/pkg/config"
+	"crypto/tls"
+	"fmt"
+	"log"
 	"os"
-
-	//"reflect"
+	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc/credentials"
 )
 
 func TestGetAgentClient(t *testing.T) {
+
+	currentdir, err := os.Getwd()
+	if err != nil {
+		log.Println("Error while getting cuerent dir", err)
+	}
+	presentdir, err := getRelativePathUpTo(currentdir)
+
+	if err != nil {
+		log.Println("Error while getting working dir", err)
+	}
+
 	type args struct {
-		config config.CaptenConfig
+		config      config.CaptenConfig
+		clusterconf config.CaptenClusterValues
 	}
 	tests := []struct {
 		name    string
@@ -23,13 +39,19 @@ func TestGetAgentClient(t *testing.T) {
 		wantErr bool
 	}{
 		{
+
 			name: "Secure connection",
 			args: args{
 				config: config.CaptenConfig{
-					AgentSecure:   true,
-					AgentHostName: "captenagent",
-					// DomainName:      "com",
-					// CaptenAgentPort: "50051",
+					AgentSecure:        true,
+					AgentHostName:      "captenagent",
+					CertDirPath:        "/" + presentdir + "/cert/",
+					ClientKeyFileName:  "client.key",
+					ClientCertFileName: "client.crt",
+					CAFileName:         "ca.crt",
+				},
+				clusterconf: config.CaptenClusterValues{
+					DomainName: "aws.optimizor.app",
 				},
 			},
 			wantErr: false,
@@ -42,6 +64,9 @@ func TestGetAgentClient(t *testing.T) {
 					AgentHostName: "captenagent",
 					// DomainName:      "com",
 					// CaptenAgentPort: "50051",
+				},
+				clusterconf: config.CaptenClusterValues{
+					DomainName: "aws.optimizor.app",
 				},
 			},
 			wantErr: false,
@@ -68,6 +93,17 @@ func TestGetAgentClient(t *testing.T) {
 }
 
 func TestGetVaultClient(t *testing.T) {
+
+	currentdir, err := os.Getwd()
+	if err != nil {
+		log.Println("Error while getting cuerent dir", err)
+	}
+	dir, err := getRelativePathUpTo(currentdir)
+
+	if err != nil {
+		log.Println("Error while getting working dir", err)
+	}
+
 	type args struct {
 		config config.CaptenConfig
 	}
@@ -81,8 +117,12 @@ func TestGetVaultClient(t *testing.T) {
 			name: "Secure connection",
 			args: args{
 				config: config.CaptenConfig{
-					AgentSecure:   true,
-					AgentHostName: "captenagent",
+					AgentSecure:        true,
+					AgentHostName:      "captenagent",
+					CertDirPath:        "/" + dir + "/cert/",
+					ClientKeyFileName:  "client.key",
+					ClientCertFileName: "client.crt",
+					CAFileName:         "ca.crt",
 				},
 			},
 			wantErr: false,
@@ -127,80 +167,233 @@ func TestGetVaultClient(t *testing.T) {
 
 }
 
-func TestLoadTLSCredentials(t *testing.T) {
-	// Test case 1: LoadX509KeyPair fails
-	captenConfig := config.CaptenConfig{
-		//CertDirPath:    "/path/to/certs",
-		ClientCertFileName: "client.crt",
-		ClientKeyFileName:  "client.key",
-		CAFileName:         "ca.crt",
+// func TestLoadTLSCredentials(t *testing.T) {
+// 	// Test case 1: LoadX509KeyPair fails
+
+// 	currentdir, err := os.Getwd()
+// 	if err != nil {
+// 		log.Println("Error while getting cuerent dir", err)
+// 	}
+// 	dir, err := getRelativePathUpTo(currentdir)
+
+// 	if err != nil {
+// 		log.Println("Error while getting working dir", err)
+// 	}
+
+// 	captenConfig := config.CaptenConfig{
+// 		CertDirPath:        "/" + dir + "/cert",
+// 		ClientCertFileName: "client.crt",
+// 		ClientKeyFileName:  "client.key",
+// 		CAFileName:         "ca.crt",
+// 	}
+// 	os.MkdirAll(captenConfig.CertDirPath, os.ModePerm)
+// 	defer os.RemoveAll(captenConfig.CertDirPath)
+
+// 	_, err = loadTLSCredentials(captenConfig)
+// 	if err == nil {
+// 		t.Errorf("Expected error, got nil")
+// 	}
+
+// 	// Test case 2: AppendCertsFromPEM fails
+// 	certFile := captenConfig.PrepareFilePath(captenConfig.CertDirPath, captenConfig.ClientCertFileName)
+// 	keyFile := captenConfig.PrepareFilePath(captenConfig.CertDirPath, captenConfig.ClientKeyFileName)
+// 	caFile := captenConfig.PrepareFilePath(captenConfig.CertDirPath, captenConfig.CAFileName)
+
+// 	err = os.WriteFile(certFile, []byte("dummy cert"), os.ModePerm)
+// 	if err != nil {
+// 		t.Fatalf("Failed to write cert file: %v", err)
+// 	}
+
+// 	err = os.WriteFile(keyFile, []byte("dummy key"), os.ModePerm)
+// 	if err != nil {
+// 		t.Fatalf("Failed to write key file: %v", err)
+// 	}
+
+// 	err = os.WriteFile(caFile, []byte("dummy ca"), os.ModePerm)
+// 	if err != nil {
+// 		t.Fatalf("Failed to write ca file: %v", err)
+// 	}
+
+// 	_, err = loadTLSCredentials(captenConfig)
+// 	if err == nil {
+// 		t.Errorf("Expected error, got nil")
+// 	}
+
+// 	// Test case 3: Successful load
+// 	certPEM, err := os.ReadFile("cert/cert.pem")
+// 	if err != nil {
+// 		t.Fatalf("Failed to read cert file: %v", err)
+// 	}
+
+// 	keyPEM, err := os.ReadFile("cert/key.pem")
+// 	if err != nil {
+// 		t.Fatalf("Failed to read key file: %v", err)
+// 	}
+
+// 	caPEM, err := os.ReadFile("cert/ca.pem")
+// 	if err != nil {
+// 		t.Fatalf("Failed to read ca file: %v", err)
+// 	}
+
+// 	err = os.WriteFile(certFile, certPEM, os.ModePerm)
+// 	if err != nil {
+// 		t.Fatalf("Failed to write cert file: %v", err)
+// 	}
+
+// 	err = os.WriteFile(keyFile, keyPEM, os.ModePerm)
+// 	if err != nil {
+// 		t.Fatalf("Failed to write key file: %v", err)
+// 	}
+
+// 	err = os.WriteFile(caFile, caPEM, os.ModePerm)
+// 	if err != nil {
+// 		t.Fatalf("Failed to write ca file: %v", err)
+// 	}
+
+// 	_, err = loadTLSCredentials(captenConfig)
+// 	if err != nil {
+// 		t.Errorf("Expected no error, got %v", err)
+// 	}
+// }
+
+func getRelativePathUpTo(currentPath string) (string, error) {
+	targetDir := "capten"
+	// Split the path into parts
+	parts := strings.Split(currentPath, string(filepath.Separator))
+
+	// Traverse the path parts and look for the target directory
+	for i, part := range parts {
+		if part == targetDir {
+			// Join the parts up to the target directory
+			return filepath.Join(parts[:i+1]...), nil
+		}
 	}
-	os.MkdirAll(captenConfig.CertDirPath, os.ModePerm)
-	defer os.RemoveAll(captenConfig.CertDirPath)
 
-	_, err := loadTLSCredentials(captenConfig)
-	if err == nil {
-		t.Errorf("Expected error, got nil")
-	}
+	return "", fmt.Errorf("directory %s not found in path %s", targetDir, currentPath)
+}
 
-	// Test case 2: AppendCertsFromPEM fails
-	certFile := captenConfig.PrepareFilePath(captenConfig.CertDirPath, captenConfig.ClientCertFileName)
-	keyFile := captenConfig.PrepareFilePath(captenConfig.CertDirPath, captenConfig.ClientKeyFileName)
-	caFile := captenConfig.PrepareFilePath(captenConfig.CertDirPath, captenConfig.CAFileName)
+func Test_loadTLSCredentials(t *testing.T) {
 
-	err = os.WriteFile(certFile, []byte("dummy cert"), os.ModePerm)
+	currentdir, err := os.Getwd()
 	if err != nil {
-		t.Fatalf("Failed to write cert file: %v", err)
+		log.Println("Error while getting cuerent dir", err)
 	}
+	dir, err := getRelativePathUpTo(currentdir)
 
-	err = os.WriteFile(keyFile, []byte("dummy key"), os.ModePerm)
 	if err != nil {
-		t.Fatalf("Failed to write key file: %v", err)
+		log.Println("Error while getting working dir", err)
 	}
 
-	err = os.WriteFile(caFile, []byte("dummy ca"), os.ModePerm)
-	if err != nil {
-		t.Fatalf("Failed to write ca file: %v", err)
+	type args struct {
+		captenConfig config.CaptenConfig
 	}
 
-	_, err = loadTLSCredentials(captenConfig)
-	if err == nil {
-		t.Errorf("Expected error, got nil")
-	}
+	tests := []struct {
+		name    string
+		args    args
+		want    credentials.TransportCredentials
+		wantErr bool
+	}{
+		{
+			name: "valid captenConfig",
+			args: args{
+				captenConfig: config.CaptenConfig{
+					AgentCertFileName: "agent.crt",
+					AgentKeyFileName:  "agent.key",
+					CAFileName:        "ca.crt",
+					//CertDirPath: ,
+					CertDirPath:        "/" + dir + "/cert/",
+					ClientCertFileName: "client.crt",
+					ClientKeyFileName:  "client.key",
+				},
+			},
+			want: credentials.NewTLS(&tls.Config{
+				// Certificates: []tls.Certificate{cert},
+				// RootCAs:      caCertPool,
+				ClientAuth: tls.RequireAnyClientCert,
+				// ClientCAs:    caCertPool,
+			}),
 
-	// Test case 3: Successful load
-	certPEM, err := os.ReadFile("testdata/cert.pem")
-	if err != nil {
-		t.Fatalf("Failed to read cert file: %v", err)
+			wantErr: true,
+		},
+		{
+			name: "invalid cert file",
+			args: args{
+				captenConfig: config.CaptenConfig{
+					AgentCertFileName:  "client.crt",
+					AgentKeyFileName:   "client.key",
+					CAFileName:         "ca.crt",
+					CertDirPath:        "/" + dir + "/cert/",
+					ClientCertFileName: "dbcjd.key",
+					ClientKeyFileName:  "client.key",
+				},
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		// {
+		// 	name: "invalid key file",
+		// 	args: args{
+		// 		captenConfig: config.CaptenConfig{
+		// 			AgentCertFileName:  "agent.crt",
+		// 			AgentKeyFileName:   "client.key",
+		// 			CAFileName:         "ca.crt",
+		// 			CertDirPath:        "/" + dir + "/cert/",
+		// 			ClientCertFileName: "client.crt",
+		// 			ClientKeyFileName:  "agent.key",
+		// 		},
+		// 	},
+		// 	want:    nil,
+		// 	wantErr: true,
+		// },
+		// {
+		// 	name: "invalid ca file",
+		// 	args: args{
+		// 		captenConfig: config.CaptenConfig{
+		// 			AgentCertFileName:  "agent.crt",
+		// 			AgentKeyFileName:   "agent.key",
+		// 			CAFileName:         "root.crt",
+		// 			CertDirPath:        "/" + dir + "/cert/",
+		// 			ClientCertFileName: "client.crt",
+		// 			ClientKeyFileName:  "client.key",
+		// 		},
+		// 	},
+		// 	want:    nil,
+		// 	wantErr: true,
+		// },
 	}
-
-	keyPEM, err := os.ReadFile("testdata/key.pem")
-	if err != nil {
-		t.Fatalf("Failed to read key file: %v", err)
-	}
-
-	caPEM, err := os.ReadFile("testdata/ca.pem")
-	if err != nil {
-		t.Fatalf("Failed to read ca file: %v", err)
-	}
-
-	err = os.WriteFile(certFile, certPEM, os.ModePerm)
-	if err != nil {
-		t.Fatalf("Failed to write cert file: %v", err)
-	}
-
-	err = os.WriteFile(keyFile, keyPEM, os.ModePerm)
-	if err != nil {
-		t.Fatalf("Failed to write key file: %v", err)
-	}
-
-	err = os.WriteFile(caFile, caPEM, os.ModePerm)
-	if err != nil {
-		t.Fatalf("Failed to write ca file: %v", err)
-	}
-
-	_, err = loadTLSCredentials(captenConfig)
-	if err != nil {
-		t.Errorf("Expected no error, got %v", err)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := loadTLSCredentials(tt.args.captenConfig)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("loadTLSCredentials() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			// if !reflect.DeepEqual(got, tt.want) {
+			// 	t.Errorf("loadTLSCredentials() = %v, want %v", got, tt.want)
+			// }
+		})
 	}
 }
+
+// 	tests := []struct {
+// 		name    string
+// 		args    args
+// 		want    credentials.TransportCredentials
+// 		wantErr bool
+// 	}{
+// 		// TODO: Add test cases.
+// 	}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			got, err := loadTLSCredentials(tt.args.captenConfig)
+// 			if (err != nil) != tt.wantErr {
+// 				t.Errorf("loadTLSCredentials() error = %v, wantErr %v", err, tt.wantErr)
+// 				return
+// 			}
+// 			if !reflect.DeepEqual(got, tt.want) {
+// 				t.Errorf("loadTLSCredentials() = %v, want %v", got, tt.want)
+// 			}
+// 		})
+// 	}
+// }
